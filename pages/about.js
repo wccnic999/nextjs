a13058a40865ed3291a0cec086711b1c4c5d2418
Nic Wong <wccnic999@hotmail.com>
2020-02-06 19:26:36 +0800

    first commit

---
+++ pages/about.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Head from 'next/head'
+
+class About extends React.Component {
+    render() {
+        return (
+        <div>
+            <Head>
+                <title>FUCK ABOUT</title>
+            </Head>
+            <div>
+                Daddy is coming
+            </div>
+        </div>
+        )
+    }
+}
+
+export default About
